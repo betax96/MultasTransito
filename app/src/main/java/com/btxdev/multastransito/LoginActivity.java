a50ee57cd571504042396661678de1ec4e62d8e8
EndersonPro <enjaviga@gmail.com>
2020-10-21 00:18:58 -0500

    feat(login): añadiendo login, configurando SharedPreferences :ok_hand:

--- app/src/main/java/com/btxdev/multastransito/LoginActivity.java
+++ app/src/main/java/com/btxdev/multastransito/LoginActivity.java
@@ -2,13 +2,58 @@ package com.btxdev.multastransito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText textEmail, textPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textEmail = (EditText) findViewById(R.id.txtEmail);
+        textPassword = (EditText) findViewById(R.id.txtPassword);
+
+        currentUser();
+
+        Button btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final String email = textEmail.getText().toString();
+        final String password = textPassword.getText().toString();
+        // TODO: Consultar a la DB en caso de necesitarlo... xD
+        savePreferencesSignIn();
+        moveToOtherActivity(MultasActivity.class);
+    }
+
+    public void currentUser () {
+        SharedPreferences sharedPreferences = getSharedPreferences("login_preference",Context.MODE_PRIVATE);
+        boolean isLogged = sharedPreferences.getBoolean("isLogged", false);
+        if (isLogged) moveToOtherActivity(MultasActivity.class);
+    }
+
+    public void moveToOtherActivity (Class<?> cls) {
+        Intent intent = new Intent();
+        intent.setClass(this, cls);
+        startActivity(intent);
+        this.finish();
+    }
+
+    private void savePreferencesSignIn () {
+        SharedPreferences sharedPreferences = getSharedPreferences("login_preference",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("isLogged", true);
+        editor.apply();
     }
 }
--- app/src/main/java/com/btxdev/multastransito/MultasActivity.java
+++ app/src/main/java/com/btxdev/multastransito/MultasActivity.java
@@ -1,6 +1,10 @@
 package com.btxdev.multastransito;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +15,16 @@ public class MultasActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multas);
+    }
+
+    public void logUp(View view) {
+        SharedPreferences sharedPreferences = MultasActivity.this.getSharedPreferences("login_preference", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("isLogged", false);
+        editor.apply();
+        Intent intent = new Intent();
+        intent.setClass(this, LoginActivity.class);
+        startActivity(intent);
+        this.finish();
     }
 }
